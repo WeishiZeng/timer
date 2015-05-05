660cfa149b8157eee41d4826f50f6092e2079c6e
Weishi Zeng <amwish.zeng@gmail.com>
2015-05-05 00:21:45 -0700

    timer start function

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    @IBOutlet weak var countDown: UILabel!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var seconds: NSTimeInterval!
+    var timer: NSTimer = NSTimer()
+    var counter:Int = 0
+    
+    @IBAction func stopButton(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func start(sender: AnyObject) {
+        
+        datePicker.datePickerMode = UIDatePickerMode.CountDownTimer
+        seconds = datePicker.countDownDuration
+        counter = Int(seconds)
+        label.text = seconds.description
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: true)
+        
+    }
+    
+    func updateCounter() {
+        
+        countDown.text = String(counter--)
+        label.text = __FUNCTION__
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    
 }
 
